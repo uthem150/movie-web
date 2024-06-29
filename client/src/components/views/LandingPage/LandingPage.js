17c8e241b2ab7305b0cc88f7bcb7c9d3a3bea9e7
uthem150 <uthem150@gmail.com>
2024-06-29 17:13:59 +0900

    1위 영화 포스터 background image로 컴포넌트화

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,20 +1,36 @@
 import React, { useEffect, useState } from "react";
 import { FaCode } from "react-icons/fa";
-import { API_URL, API_KEY } from "../../Config";
+import { API_URL, API_KEY, IMAGE_BASE_URL } from "../../Config";
+import MainImage from "./Sections/MainImage";
 
 function LandingPage() {
+  const [movies, setMovies] = useState([]); // 인기 영화들 담을 배열
+  const [mainImage, setMainImage] = useState(null); // 가장 인기있는 영화의 이미지
+
   // 영화 정보 가져오기
   useEffect(() => {
     const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`; //20개의 가장 유명한 영화들 가져옴
 
     fetch(endpoint)
       .then((response) => response.json()) //json메소드로 받아온 응답을 json화
-      .then((response) => console.log(response)); //json화 한 응답을 가져옴
+      .then((response) => {
+        // console.log(response);
+        setMovies([...response.results]); //받은 응답을 movies에 저장
+        setMainImage(response.results[0]);
+      });
   }, []);
 
   return (
     <div style={{ width: "100%", margin: "0" }}>
-      {/* main image */}
+      {/* mainImage를 받아온다면, 이미지 컴포넌트를 생성하라. */}
+      {mainImage && (
+        <MainImage
+          image={`${IMAGE_BASE_URL}w1280${mainImage.backdrop_path}`}
+          title={mainImage.original_title}
+          text={mainImage.overview}
+        />
+      )}
+
       <div style={{ width: "85%", margin: "1rem auto" }}>
         <h2>Movies by latest</h2>
         <hr />
