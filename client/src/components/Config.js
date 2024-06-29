8c0c3920d47154da5bbc9a1bfca5c58fbdfd0976
uthem150 <uthem150@gmail.com>
2024-06-29 14:52:24 +0900

    API 중복되는 URL 상수화

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,6 @@
 //SERVER ROUTES
-export const USER_SERVER = '/api/users';
+export const USER_SERVER = "/api/users";
+
+export const API_URL = "https://api.themoviedb.org/3/"; //URL에서 중복되는 부분 상수처리
+
+export const IMAGE_BASE_URL = "https://image.tmdb.org./t/p/"; //이미지 가져오는 URL에서 중복되는 부분 상수처리
