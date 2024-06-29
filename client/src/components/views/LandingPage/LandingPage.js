e6b42e02ef44cef3c6a5f08c3f318a7b6e4dacdf
uthem150 <uthem150@gmail.com>
2024-06-29 17:14:43 +0900

    API 한국어 설정

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -9,7 +9,7 @@ function LandingPage() {
 
   // 영화 정보 가져오기
   useEffect(() => {
-    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`; //20개의 가장 유명한 영화들 가져옴
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=ko&page=1`; //20개의 가장 유명한 영화들 가져옴
 
     fetch(endpoint)
       .then((response) => response.json()) //json메소드로 받아온 응답을 json화
