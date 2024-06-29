78fe04ade9322d7c5712e61963fe1c618ff9d35b
uthem150 <uthem150@gmail.com>
2024-06-29 16:24:20 +0900

    fetch로 영화정보 가져오기

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import { FaCode } from "react-icons/fa";
+import { API_URL, API_KEY } from "../../Config";
 
 function LandingPage() {
-    return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-            </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
-    )
+  // 영화 정보 가져오기
+  useEffect(() => {
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`; //20개의 가장 유명한 영화들 가져옴
+
+    fetch(endpoint)
+      .then((response) => response.json()) //json메소드로 받아온 응답을 json화
+      .then((response) => console.log(response)); //json화 한 응답을 가져옴
+  }, []);
+
+  return (
+    <div style={{ width: "100%", margin: "0" }}>
+      {/* main image */}
+      <div style={{ width: "85%", margin: "1rem auto" }}>
+        <h2>Movies by latest</h2>
+        <hr />
+
+        {/* movie grid cards */}
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <button>Load more</button>
+      </div>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
