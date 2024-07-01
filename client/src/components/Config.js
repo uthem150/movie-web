ff7ccd86e24e24fba9af97e0efde15bffb117d79
uthem150 <uthem150@gmail.com>
2024-07-01 16:44:24 +0900

    Updates

--- client/src/components/Config.js
+++
@@ -1,6 +0,0 @@
-//SERVER ROUTES
-export const USER_SERVER = "/api/users";
-
-export const API_KEY = "a2d9d8e022b43127fd4f16913a183eba";
-export const API_URL = "https://api.themoviedb.org/3/"; //URL에서 중복되는 부분 상수처리
-export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/"; //이미지 가져오는 URL에서 중복되는 부분 상수처리
